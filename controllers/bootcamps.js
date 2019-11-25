47bbbe76b4f7cb316112985406413511ada76fe4
hamid-shahmohammadi <hamid@shahmohamadi@gmail.com>
2019-11-25 09:58:14 +0330

    6.1-8

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,16 +1,62 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler=require('../middleware/async');
+const geocoder = require('../utils/geocoder');
 const Bootcamp = require('../models/Bootcamp');
 
 // @desc get all
 // @route GET /api/v1/bootcamps
 // @access Public
 exports.getBootcamps = asyncHandler(async (req,res,next)=>{
+    let query;
+
+    const reqQuery={...req.query};
+
+    const removeFields=['select','sort','page','limit'];
+
+    removeFields.forEach(param => delete reqQuery[param]);
     
-       const bootcamps = await Bootcamp.find();  
-       res.status(200).json({success:true,count:bootcamps.length,data:bootcamps});   
-  
+    let queryStr=JSON.stringify(reqQuery);
+    queryStr=queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g,match => `$${match}`);
+    query=Bootcamp.find(JSON.parse(queryStr)).populate('courses');
+
+    //select field
+    if(req.query.select){
+        const fields = req.query.select.split(',').join(' ');
+        query=query.select(fields);   
+    }
+    //sort
+    if(req.query.sort){
+        const sortBy=req.query.sort.split(',').join(' ');
+        query=query.sort(sortBy);
+    }else{
+        query=query.sort('-createdAt');
+    }
+    //pagination
+    const page=parseInt(req.query.page,10) || 1;
+    const limit=parseInt(req.query.limit,10) || 25;
     
+    const startIndex=(page-1) * limit;
+    const endIndex=page*limit;
+    const total= await Bootcamp.countDocuments();
+
+    query = query.skip(startIndex).limit(limit);
+    
+    const bootcamps = await query;  
+    // Paginatin result
+    const pagination={};
+    if(endIndex < total){
+        pagination.next={
+            page:page+1,
+            limit
+        }
+    }
+    if(startIndex > 0){
+        pagination.prev={
+            page:page-1,
+            limit
+        }
+    }
+    res.status(200).json({success:true,count:bootcamps.length,pagination,data:bootcamps});    
 });
 
 // @desc single
@@ -63,14 +109,42 @@ exports.updateBootcamp = asyncHandler(async (req,res,next)=>{
 // @access private
 exports.deleteBootcamp = asyncHandler(async (req,res,next)=>{
    
-        const bootcamp= await Bootcamp.findByIdAndDelete(req.params.id);
+        const bootcamp= await Bootcamp.findById(req.params.id);
         if(!bootcamp){
             return next(
                 new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`,404)
             );
-            
-            // return res.status(400).json({success:flase});
         }
+        bootcamp.remove();
         res.status(200).json({success:true,data:{}});
     
+});
+
+// @desc   get bootcamps within a radius
+// @route  get /api/v1/bootcamps/radius/:zipcode/:distance
+// @access private
+exports.getBootcampsInRadius = asyncHandler(async (req,res,next)=>{
+   const {zipcode,distance}=req.params;
+   
+
+   // Get lat/lng geocoder
+   const loc=await geocoder.geocode(zipcode);
+   
+   const lat=loc[0].latitude;
+   const lng=loc[0].longitude;
+
+   // calc radius using radiuns
+   // divide dist by radius of earth
+   //earth radius=3.963mi / 6,378 km
+   const radius = distance / 3963;
+   
+   const bootcamps = await Bootcamp.find({
+       location:{ $geoWithin: {$centerSphere:[[lng,lat],radius]}}
+    });
+    console.log(lng,lat,radius);
+   res.status(200).json({
+       success:true,
+       count:bootcamps.length,
+       data:bootcamps
+   });
 });
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,7 +1,16 @@
 const express=require('express');
-const { getBootcamps,getBootcamp,createBootcamp,updateBootcamp,deleteBootcamp}=require('../controllers/bootcamps');
+const { getBootcamps,getBootcamp,createBootcamp,updateBootcamp,
+    deleteBootcamp,getBootcampsInRadius}=require('../controllers/bootcamps');
+
+// include other resource routers
+const courseRouter=require('./courses');
+
 const router=express.Router();
 
+// Re-route into other resource router
+router.use('/:bootcampId/courses',courseRouter);
+
+router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 router.route('/').get(getBootcamps).post(createBootcamp);
 router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp);
 
